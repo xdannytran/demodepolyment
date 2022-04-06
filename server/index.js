68598ed1a48ad04f3cf10e2f11fdcb49c1765667
Danny Tran <xdannytran@hotmail.com>
2022-04-06 11:55:06 -0500

    init

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.static('public'))
+app.use(express.static('Public'))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
